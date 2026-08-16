bb8ef29471955aad9ce00b8b9fd4d787b72160db
Himanish106 <himanish.das23@gmail.com>
2026-08-16 14:19:23 +0530

    Restore

--- Github_Day5/deploy.sh
+++ Github_Day5/deploy.sh
@@ -2,6 +2,3 @@ echo "Deploying Azure Infrastructure..."
 echo "Creating Resource Group: rg-production-eastus"
 echo "Creating Virtual Network: vnet-DEV-TESTING-eastus"
 echo "Creating Subnet: snet-app-eastus"
-
-# Test Secret
-DUMMY_TOKEN="ghp_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" 
