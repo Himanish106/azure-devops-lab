8332a5bc4261fc5a678d11ec111aee1393cd7299
Himanish106 <himanish.das23@gmail.com>
2026-08-16 14:26:44 +0530

    Test

--- Github_Day5/deploy.sh
+++ Github_Day5/deploy.sh
@@ -3,3 +3,5 @@ echo "Creating Resource Group: rg-production-eastus"
 echo "Creating Virtual Network: vnet-DEV-TESTING-eastus"
 echo "Creating Subnet: snet-app-eastus"
 DUMMY_TOKEN="ghp_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+DUMMY_TOKEN2 = "ghp_000000000000000000000000000000000000"
