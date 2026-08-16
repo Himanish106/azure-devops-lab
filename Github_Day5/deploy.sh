606deea3d2b0666a4bc75d2aec1ef311d8d6ceb5
Himanish106 <himanish.das23@gmail.com>
2026-08-16 14:14:19 +0530

    check security

--- Github_Day5/deploy.sh
+++ Github_Day5/deploy.sh
@@ -2,3 +2,6 @@ echo "Deploying Azure Infrastructure..."
 echo "Creating Resource Group: rg-production-eastus"
 echo "Creating Virtual Network: vnet-DEV-TESTING-eastus"
 echo "Creating Subnet: snet-app-eastus"
+
+# Test Secret
+DUMMY_TOKEN="ghp_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" 
